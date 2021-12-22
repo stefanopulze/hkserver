fd982333a066a79298fd18d5057871b2111e9db5
stefano.pulze87@gmail.com <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>
2021-12-22 11:32:18 +0100

    Fix script version

--- scripts/build-bin.sh
+++ scripts/build-bin.sh
@@ -3,7 +3,7 @@
 GOOS="linux"
 GOARCH="arm64"
 #GOARCH="amd64"
-VERSION="0.0.2"
+VERSION=$(cat ./VERSION)
 
 env GOOS="$GOOS" GOARCH="$GOARCH" \
   go build -ldflags="-w -s -X configs.Version=$VERSION" -o ./output/hkserver .
